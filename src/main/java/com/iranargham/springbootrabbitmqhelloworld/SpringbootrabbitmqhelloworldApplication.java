e8b016b63099131f49209509ac4b2386dcebcb5a
f.mohebbi <faribamhb>
2020-09-26 17:51:53 +0330

    first

---
+++ src/main/java/com/iranargham/springbootrabbitmqhelloworld/SpringbootrabbitmqhelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.iranargham.springbootrabbitmqhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrabbitmqhelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootrabbitmqhelloworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iranargham/springbootrabbitmqhelloworld/SpringbootrabbitmqhelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iranargham.springbootrabbitmqhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrabbitmqhelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
